89bed4c065abb899e9f26fae8964e22ad6f9f6f1
SMB-M87 <SMB-M87@proton.me>
2025-11-12 20:27:27 +0100

    Alpha patch

--- config.def.h
+++ config.def.h
@@ -1,6 +1,6 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
-
+static const unsigned int alpha = 0x99; /* Amount of opacity (0xff == opaque) */
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
@@ -17,6 +17,13 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = { "#000000", "#00ffff" },
 	[SchemeCursor] = { "#222222", "#bbbbbb"},
 };
+
+static const unsigned int alphas[SchemeLast][2] = {
+	[SchemeNorm] = { OPAQUE, alpha },
+	[SchemeSel] = { OPAQUE, alpha },
+	[SchemeOut] = { OPAQUE, alpha },
+};
+
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines      = 0;
 static unsigned int columns    = 0;

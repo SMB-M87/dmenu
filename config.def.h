e9dc41a2acd513da4ff7444c3c7963735591edfc
SMB-M87 <SMB-M87@proton.me>
2026-02-13 19:00:43 +0100

    Change font to 10

--- config.def.h
+++ config.def.h
@@ -4,10 +4,10 @@ static const unsigned int alpha = 0x99; /* Amount of opacity (0xff == opaque) */
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
-static const float menu_height_ratio = 2.0f;  /* This is the ratio used in the original calculation */
+static const float menu_height_ratio = 1.0f;  /* This is the ratio used in the original calculation */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=14"
+	"monospace:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
